120e6c9d5264eae0302fa16c352ffd543ee749ba
Luis M. Gallardo D <lgallard@gmail.com>
2021-08-24 12:34:49 -0300

    Force aws_s3_bucket_public_access_block to be applied first

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_s3_bucket_public_access_block" "default" {
   ignore_public_acls      = var.ignore_public_acls
   block_public_policy     = var.block_public_policy
   restrict_public_buckets = var.restrict_public_buckets
-  depends_on              = [aws_s3_bucket.default, time_sleep.wait_2_mins]
+  depends_on              = [aws_s3_bucket.default]
 
 }
 
